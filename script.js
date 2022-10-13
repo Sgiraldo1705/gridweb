dd38d86535a2e9d5b9d74792738d1ca8effb443f
Santiago <santiago.giraldomesa@gmail.com>
2022-10-13 11:42:38 -0500

    added JS CSS

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-//SETS CONSTANTS AND VARIABLES
-const container = document.getElementById('container');
-let rows = document.getElementsByClassName('gridRow');
-let cells = document.getElementsByClassName('cell');
+let color = 'black';
+let click = true;
 
-//Create a default grid
-function defaultGrid() {
-  makeRows(16);
-  makeColumns(16);
-}
-//takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-  //Creates rows
-  for (r = 0; r < rowNum; r++) {
-    let row = document.createElement('div');
-    container.appendChild(row).className = 'gridRow';
+function createGrid(size) {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+  for (c = 0; c < amount; c++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = 'white';
+    board.insertAdjacentElement('beforeend', square);
   }
 }
-//Creates Columns
-function makeColumns(cellNum) {
-  for (i = 0; i < rows.length; i++) {
-    for (j = 0; j < cellNum; j++) {
-      let newCell = document.createElement('div');
-      rows[j].appendChild(newCell).className = 'cell';
+
+createGrid(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    document.querySelector('.error').style.display = 'none';
+    createGrid(input);
+  } else {
+    document.querySelector('.error').style.display = 'flex';
+  }
+}
+
+function colorSquare() {
+  if (click) {
+    if (color === 'random') {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
     }
   }
 }
+function changeColor(choice) {
+  color = choice;
+}
+function resetBoard() {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => (div.style.backgroundColor = 'white'));
+}
+document.querySelector('body').addEventListener('click', (e) => {
+  if (e.target.tagName != 'BUTTON') {
+    click = !click;
+    if (click) {
+      document.querySelector('.mode').textContent = 'Mode: Coloring';
+    } else {
+      document.querySelector('.mode').textContent = 'Mode: NOT Coloring';
+    }
+  }
+});
